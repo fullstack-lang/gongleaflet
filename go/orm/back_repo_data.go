6c6b12aae7c6c18840d1df0d77caca384dcd12ec
peugeot <thomaspeugeot@yahoo.fr>
2024-11-08 08:18:30 +0100

    new gongc & go get -u ./...

--- go/orm/back_repo_data.go
+++ go/orm/back_repo_data.go
@@ -24,6 +24,11 @@ type BackRepoData struct {
 }
 
 func CopyBackRepoToBackRepoData(backRepo *BackRepoStruct, backRepoData *BackRepoData) {
+
+	// wait till backRepo is written by commit
+	backRepo.rwMutex.RLock()
+	defer backRepo.rwMutex.RUnlock()
+
 	// insertion point for slices copies
 	for _, circleDB := range backRepo.BackRepoCircle.Map_CircleDBID_CircleDB {
 
