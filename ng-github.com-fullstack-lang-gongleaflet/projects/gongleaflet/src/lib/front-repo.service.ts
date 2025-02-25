2018d8b8b7307fe9e140650e643b340aa7945b18
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-02-26 00:20:42 +0100

    new gongc

--- ng-github.com-fullstack-lang-gongleaflet/projects/gongleaflet/src/lib/front-repo.service.ts
+++ ng-github.com-fullstack-lang-gongleaflet/projects/gongleaflet/src/lib/front-repo.service.ts
@@ -232,7 +232,7 @@ export class FrontRepoService {
 	}
 
 	// typing of observable can be messy in typescript. Therefore, one force the type
-	observableFrontRepo: [
+	observableFrontRepo!: [
 		Observable<null>, // see below for the of(null) observable
 		// insertion point sub template 
 		Observable<CircleAPI[]>,
@@ -244,26 +244,7 @@ export class FrontRepoService {
 		Observable<UserClickAPI[]>,
 		Observable<VLineAPI[]>,
 		Observable<VisualTrackAPI[]>,
-	] = [
-			// Using "combineLatest" with a placeholder observable.
-			//
-			// This allows the typescript compiler to pass when no GongStruct is present in the front API
-			//
-			// The "of(null)" is a "meaningless" observable that emits a single value (null) and completes.
-			// This is used as a workaround to satisfy TypeScript requirements and the "combineLatest" 
-			// expectation for a non-empty array of observables.
-			of(null), // 
-			// insertion point sub template
-			this.circleService.getCircles(this.GONG__StackPath, this.frontRepo),
-			this.diviconService.getDivIcons(this.GONG__StackPath, this.frontRepo),
-			this.layergroupService.getLayerGroups(this.GONG__StackPath, this.frontRepo),
-			this.layergroupuseService.getLayerGroupUses(this.GONG__StackPath, this.frontRepo),
-			this.mapoptionsService.getMapOptionss(this.GONG__StackPath, this.frontRepo),
-			this.markerService.getMarkers(this.GONG__StackPath, this.frontRepo),
-			this.userclickService.getUserClicks(this.GONG__StackPath, this.frontRepo),
-			this.vlineService.getVLines(this.GONG__StackPath, this.frontRepo),
-			this.visualtrackService.getVisualTracks(this.GONG__StackPath, this.frontRepo),
-		];
+	];
 
 	//
 	// pull performs a GET on all struct of the stack and redeem association pointers 
